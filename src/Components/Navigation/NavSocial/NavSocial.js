62241821e95f7d00169b1f3f29230bcc928c6dcc
Ambaliya Dhruv <ambaliyadhruv1112@gmail.com>
2022-03-18 11:34:24 +0530

    size change

--- src/Components/Navigation/NavSocial/NavSocial.js
+++ src/Components/Navigation/NavSocial/NavSocial.js
@@ -30,8 +30,41 @@ const NavSocial = ({ socialClass, socialIcons }) => {
                 >
                     <FontAwesomeIcon
                         icon={["fab", "linkedin"]}
-                        className={`${styles[socialIcons]}`}
+                       className={`${styles[socialIcons]}`}
                     />
+                </a>
+            </li>
+            <li className={`${styles[socialClass]}`}>
+                <a
+                    href="mailto:ambaliyadhruv1112@gmail.com"
+                    className="socialIcons"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                    aria-label="My Linkedin email."
+                >
+                    <FontAwesomeIcon icon="envelope"  className={`${styles[socialIcons]}`} />
+                </a>
+            </li>
+            <li className={`${styles[socialClass]}`}>
+                <a
+                    href="tel:+918264854885"
+                    className="socialIcons"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                    aria-label="My Linkedin contact"
+                >
+                    <FontAwesomeIcon icon="phone"  className={`${styles[socialIcons]}`} />
+                </a>
+            </li>
+            <li className={`${styles[socialClass]}`}>
+                <a
+                    href="https://drive.google.com/file/d/1g20D1sblle14ngNcesAZdxeyTvOhlK7t/view?usp=sharing"
+                    className="socialIcons"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                    aria-label="My Linkedin resume"
+                >
+                    <FontAwesomeIcon icon="file"  className={`${styles[socialIcons]}`} />
                 </a>
             </li>
         </>
