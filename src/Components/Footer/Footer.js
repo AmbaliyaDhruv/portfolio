54bf86a5e8d035a4edf7ae876e71685876c0fcae
Ambaliya Dhruv <ambaliyadhruv1112@gmail.com>
2022-03-18 11:49:36 +0530

    add profile in mobile view

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,52 +6,21 @@ import styles from "./Footer.module.css";
 const Footer = () => {
     const { isLight } = useContext(ThemeContext);
     const theme = !isLight ? styles.dark : "";
-
-    // window.usabilla_live("setEventCallback", function(category, action, label, value) {
-    //     if(action !== "Campaign:Open") {
-    //         return;
-    //     }
-    
-    //     window.addEventListener("message", function(event) {
-    //         // Listen to messages from the Usabilla Cloudfront domain
-    //         if(!/d6tizftlrpuof\.cloudfront\.net/.test(event.origin)) {
-    //             return;
-    //         }
-    
-    //         try {
-    //             var data = JSON.parse(event.data);
-    //             // On the final page
-    
-    //             if(data.type === "pageSwitch" && data.end) {
-    //                 for(var key in data.data) {
-    //                     ga('gtag_UA_178920862_3' + '.send', 'event', 
-    //                     'Usabilla Campaign - ' + label, // event category
-    //                     key, // event action
-    //                     data.data[key] // event label
-    //                     );
-                      
-    //                 }
-    //             }
-    //         } catch(e) {
-    //             // Ignore errors, usually JSON decode problems
-    //         }
-    //     });
-    // });
-
-    return (
+ return (
         <footer className={`${styles.footer} ${theme}`}>
             <ul className={styles.iconContainer}>
                 <NavSocial socialClass={""} socialIcons={"footerSocialIcons"} />
             </ul>
+          
             <div className={styles.copyright}>
+            <small>+91 82648 54885</small>
+            <br/>
+            <small>ambaliyadhruv1112@gmail.com</small>
+            <br/>
                 <small>Made with By.</small>
                 <small>&copy;2022</small>
             </div>
-            <iframe
-                src="https://www.getfeedback.com/e/gRjjRmfg"
-                style={{ border: "0"}}
-            ></iframe>
-        </footer>
+     </footer>
     );
 };
 
